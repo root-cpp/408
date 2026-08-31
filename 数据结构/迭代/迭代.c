3965b039ba95c66a75d6dcfbd771f3985e22a033
root-cpp <13283752095@163.com>
2026-08-31 16:02:14 +0800

    迭代

--- 数据结构/迭代/迭代.c
+++ 数据结构/迭代/迭代.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
+
+//for—≠ª∑
+int forLoop(int n) {
+    int res = 0;
+
+    for (int i = 1; i <= n; i++) {
+        res += i;
+    }
+    //return res;
+    printf("%d",res);
+}
+
+//while—≠ª∑
+
+
+
 int main(){
+
+//printf("%d",forLoop(100));
+forLoop(100);
 
 
 
